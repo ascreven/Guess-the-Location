3fb7e60e1e381fd00a4b6040513f9cc7df3f3376
Alexandra <akscreven@gmail.com>
2015-10-26 11:15:37 -0400

    second question working

--- project1/script.js
+++ project1/script.js
@@ -6,21 +6,56 @@ $(document).ready(function () {
 });
 
   //if they click on North America button they get they get +1 to their score, otherwise they just get one more attempt
-  var firstMove = function() {
-    $('#na').on('click', function (){
-      var right = 1;
-      var score = right + '/' + 1
-      $('.score').html(score);
-    });
-    $('.options :not(#na)').on('click', function (){
-      var right = 0
-      var score = [right, '/1']
-      $('.score').html(score);
-    });
-  };
-$(document).ready(function () {
-  firstMove();
+  $(document).ready(function () {
+    $('#next').hide();
+    firstMove();
+  });
+
+function firstMove() {
+  tries = 0;
+  right = 0;
+  $('#na').on('click', function (){
+    right++;
+    firstResults();
 });
+  $('.options :not(#na)').on('click', function (){
+    firstResults();
+  });
+}
+function firstResults() {
+  $('.options button').off();
+  $('#next').show();
+  tries++;
+  score = this.right + '/' + this.tries;
+  $('.score').html(score);
+  secondMove();
+}
+  //second round
+function secondMove() {
+    $('#next').on('click', function (){
+    $('#next').hide();
+    $('img').attr('src','zhangyeDanxiaLandformGeologicalPark.jpg');
+    $('.options button').on()
+    $('#asia').on('click', function (){
+      right++;
+      secondResults();
+    });
+    $('.options :not(#asia)').on('click',function(){
+      secondResults();
+    });
+  });
+}
+function secondResults() {
+  $('.options button').off();
+  $('#next').show();
+  tries++;
+  score = this.right + '/' + this.tries;
+  $('.score').html(score);
+  // thirdMove();
+}
+
+  //
+
   // $(this).html('next');
   // $('h2').html('Snoqualmie Falls');
   // $('img').attr('src','snoqualmie_falls.jpg');
