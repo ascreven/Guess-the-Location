4f61502bb048aeb96b2295a77d48f4bdb6545026
Alexandra <akscreven@gmail.com>
2015-10-27 10:54:13 -0400

    flow works

--- project1/script.js
+++ project1/script.js
@@ -10,11 +10,11 @@ $(document).ready(function () {
   $('.next').hide();
   tries = 0;
   right = 0;
-  score(tries,right);
-  i=0;
+  score(right, tries);
   handleChoice();
-  updateLocation(i);
-
+  locationNumber = 0;
+  updateLocation(locationNumber);
+  updateScreen();
 });
 
 // scoreboard combines global variables right and tries
@@ -75,323 +75,66 @@ var locations = [
   },
   // location 8
   {place: "Underground Jenolan Caves",
-  image: "undergroundJenolanCaves.jpg'",
+  image: "undergroundJenolanCaves.jpg",
   continentFormal: "Australia",
   continent: 'aus',
   description: ''
   },
   // location 9
-  {place: "Hanoi'",
-  image: "hanoi.jpg'",
+  {place: "Hanoi",
+  image: "hanoi.jpg",
   continentFormal: "Asia",
   continent: 'asia',
   description: ''
   },
   // location 10
-  {place: "Mount Roraima'",
-  image: "mountRoraima.jpg'",
+  {place: "Mount Roraima",
+  image: "mountRoraima.jpg",
   continentFormal: "Asia",
   continent: 'asia',
   description: ''
   },
 ];
 //change things to divs to easily hide and show
-function updateLocation(i) {
+function updateLocation() {
   $('.descriptions').hide();
   $('.outcome').hide();
-  $('.place').html(locations[i].place + '?');
-  $('.location img').attr('src', locations[i].image);
-  $('.outcome').html('You are not correct. ' + locations[i].place + ' is located in ' + locations[i].continentFormal + '.');
-  answer = locations[i].continent;
+  $('.place').html(locations[locationNumber].place + '?');
+  $('.location img').attr('src', locations[locationNumber].image);
+  $('.outcome').html('You are not correct. ' + locations[locationNumber].place + ' is located in ' + locations[locationNumber].continentFormal + '.');
+  answer = locations[locationNumber].continent;
+
 }
-function handleChoice () {
+function handleChoice() {
   $('.continent').on('click', function (){
-    debugger
     tries++;
     var selectedAnswer = $(this).attr('id');
     if (selectedAnswer==answer){
       right++;
-      $('.outcome').html('You are correct! ' + locations[i].place + ' is located in ' + locations[i].continentFormal + '.');
+      $('.outcome').html('You are correct! ' + locations[locationNumber].place + ' is located in ' + locations[locationNumber].continentFormal + '.');
     }
-    console.log(right);
   score(right,tries);
   $('h2:first-child').hide();
   $('h2.where').hide();
-  $('.place').html(locations[i].place);
+  $('.place').html(locations[locationNumber].place);
   $('.continents').hide();
-  $('.descriptions').html('Description: ' + locations[i].description);
+  $('.descriptions').html('Description: ' + locations[locationNumber].description);
   $('.descriptions').show();
   $('.outcome').show();
   $('.next').show();
-  updateScreen();
   });
 }
 
 function updateScreen() {
   $('.next').on('click', function (){
-    i++;
+    console.log(locationNumber);
+    locationNumber++;
+    console.log(locationNumber);
     $('h2.where').show();
     $('.continents').show();
     $('h2:first-child').show();
     $('.next').hide();
-    updateLocation(i);
+    updateLocation(locationNumber);
   });
+
 }
-
-
-
-// function firstMove() {
-//   $('#na').on('click', function (){
-//     right++;
-//     firstResults();
-//   });
-//   $('.options :not(#na)').on('click', function (){
-//     firstResults();
-//   });
-// }
-// function firstResults() {
-//   $('.options button').off();
-//   $('.next').show();
-//   tries++;
-//   score(tries,right);
-//   secondMove();
-// // }
-//   //second round
-// function secondMove() {
-//   $('.next').on(('click'), function (){
-//     // keep track of where next is going to
-//     $('.next').hide();
-//     $('img').attr('src','zhangyeDanxiaLandformGeologicalPark.jpg');
-//     $('h2').html('Zhangye Danxia Landform')
-//     $('.options button').on();
-//     // only get +1 if they click when it actually is Asia(or other continent for other problems)
-//     $('#asia').on('click', function (){
-//       if (tries===1){
-//         right++;
-//       }
-//       secondResults();
-//     });
-//     $('.options :not(#asia)').on('click',function(){
-//       secondResults();
-//     });
-//   })
-//   };
-//
-// function secondResults() {
-//   $('.options button').off();
-//   $('.next').show();
-//   tries++;
-//   score = this.right + '/' + this.tries;
-//   $('.score').html(score);
-//   thirdMove();
-// }
-//
-// // third round
-//
-// function thirdMove() {
-//   $('.next').on('click', function (){
-//     $('.next').hide();
-//     $('img').attr('src','rondaSpain.jpg');
-//     $('h2').html('Ronda');
-//     $('.options button').on();
-//     $('#eur').on('click', function (){
-//       if (tries==3){
-//         right++;
-//       }
-//       thirdResults();
-//     });
-//     $('.options :not(#eur)').on('click',function(){
-//       thirdResults();
-//     });
-//   });
-// }
-//
-// function thirdResults() {
-//   $('.options button').off();
-//   $('.next').show();
-//   score = right + '/' + this.tries;
-//   $('.score').html(score);
-//   fourthMove();
-// }
-//
-// function fourthMove() {
-//   $('.next').on('click', function (){
-//     $('.next').hide();
-//     $('h2').html('Torres Del Paine');
-//     $('img').attr('src','torresDelPaine.jpg');
-//     $('.options button').on();
-//     $('#sa').on('click', function (){
-//       if (tries==4){
-//         right++;
-//       }
-//         fourthResults();
-//     });
-//     $('.options :not(#sa)').on('click',function(){
-//       fourthResults();
-//     });
-//   });
-// }
-//
-// function fourthResults() {
-//   $('.options button').off();
-//   $('.next').show();
-//   score = right + '/' + this.tries;
-//   $('.score').html(score);
-//   fifthMove();
-// }
-//
-// function fifthMove() {
-//   $('.next').on('click', function (){
-//     $('.next').hide();
-//     $('h2').html('Astronomical Clock II');
-//     $('img').attr('src','astronomicalClockII.jpg');
-//     $('.options button').on();
-//     $('#eur').on('click', function (){
-//       if (tries==5){
-//         right++;
-//       }
-//         fifthResults();
-//     });
-//     $('.options :not(#eur)').on('click',function(){
-//       fifthResults();
-//     });
-//   });
-// }
-//
-// function fifthResults() {
-//   $('.options button').off();
-//   $('.next').show();
-//   score = right + '/' + this.tries;
-//   $('.score').html(score);
-//   sixthMove();
-// }
-//
-// function sixthMove() {
-//   $('.next').on('click', function (){
-//     $('.next').hide();
-//     $('h2').html('Maletsunyane Falls');
-//     $('img').attr('src','maletsunyaneFalls.jpg');
-//     $('.options button').on();
-//     $('#afr').on('click', function (){
-//       if (tries==6){
-//         right++;
-//       }
-//         sixthResults();
-//     });
-//     $('.options :not(#afr)').on('click',function(){
-//       sixthResults();
-//     });
-//   });
-// }
-//
-// function sixthResults() {
-//   $('.options button').off();
-//   $('.next').show();
-//   score = right + '/' + this.tries;
-//   $('.score').html(score);
-//   seventhMove();
-// }
-//
-// function seventhMove() {
-//   $('.next').on('click', function (){
-//     $('.next').hide();
-//     $('h2').html('Fly Geyser');
-//     $('img').attr('src','flyGeyser.jpg');
-//     $('.options button').on();
-//     $('#na').on('click', function (){
-//       if (tries==7){
-//         right++;
-//       }
-//         seventhResults();
-//     });
-//     $('.options :not(#na)').on('click',function(){
-//       seventhResults();
-//     });
-//   });
-// }
-//
-// function seventhResults() {
-//   $('.options button').off();
-//   $('.next').show();
-//   score = right + '/' + this.tries;
-//   $('.score').html(score);
-//   eighthMove();
-// }
-//
-// function eighthMove() {
-//   $('.next').on('click', function (){
-//     $('.next').hide();
-//     $('h2').html('Underground Jenolan Caves');
-//     $('img').attr('src','undergroundJenolanCaves.jpg');
-//     $('.options button').on();
-//     $('#aus').on('click', function (){
-//       if (tries==8){
-//         right++;
-//       }
-//         eighthResults();
-//     });
-//     $('.options :not(#aus)').on('click',function(){
-//       eighthResults();
-//     });
-//   });
-// }
-//
-// function eighthResults() {
-//   $('.options button').off();
-//   $('.next').show();
-//   score = right + '/' + this.tries;
-//   $('.score').html(score);
-//   ninthMove();
-// }
-//
-// function ninthMove() {
-//   $('.next').on('click', function (){
-//     $('.next').hide();
-//     $('h2').html('Hanoi');
-//     $('img').attr('src','hanoi.jpg');
-//     $('.options button').on();
-//     $('#asia').on('click', function (){
-//       if (tries==9){
-//         right++;
-//       }
-//         ninthResults();
-//     });
-//     $('.options :not(#asia)').on('click',function(){
-//       ninthResults();
-//     });
-//   });
-// }
-//
-// function ninthResults() {
-//   $('.options button').off();
-//   $('.next').show();
-//   score = right + '/' + this.tries;
-//   $('.score').html(score);
-//   tenthMove();
-// }
-//
-// function tenthMove() {
-//   $('.next').on('click', function (){
-//     $('.next').hide();
-//     $('h2').html('Mount Roraima');
-//     $('img').attr('src','mountRoraima.jpg');
-//     $('.options button').on();
-//     $('#asia').on('click', function (){
-//       if (tries==10){
-//         right++;
-//       }
-//         tenthResults();
-//     });
-//     $('.options :not(#asia)').on('click',function(){
-//       tenthResults();
-//     });
-//   });
-// }
-//
-// function tenthResults() {
-//   $('.options button').off();
-//   $('.next').html('Final Score');
-//   $('.next').show();
-//   score = right + '/' + this.tries;
-//   $('.score').html(score);
-// }
